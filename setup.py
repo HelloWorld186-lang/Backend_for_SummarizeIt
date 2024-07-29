d149e599e983a01285543e5a23ac64d2c61eb6de
ayush-git-hub <159812035+ayush-git-hub@users.noreply.github.com>
2024-07-29 17:47:35 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your-project-name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Flask==2.3.2',
+        'Flask-CORS==3.0.10',
+        'PyPDF2==3.0.1',
+        'nltk==3.8.1',
+        'sumy==0.11.0',
+        'python-pptx==0.6.23',
+        'pyttsx3==2.90',
+        'moviepy==1.0.3',
+        'Pillow==10.1.0',
+        'gunicorn==20.1.0',
+    ],
+)
